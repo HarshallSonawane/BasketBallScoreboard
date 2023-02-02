51c366d68ba4e8fdf0a4665007078dd2b82f5172
HarshallSonawane <harshalsonawane127@gmail.com>
2023-02-02 16:09:02 +0530

    FinalDraft

---
+++ index.js
@@ -0,0 +1,55 @@
+let scoreHome = document.getElementById("score-tracker-home")
+let scoreAway = document.getElementById("score-tracker-away")
+let totalHomeScore = 0 
+let totalAwayScore = 0
+
+
+function increaseByOneHome(){
+
+    totalHomeScore = totalHomeScore+1
+    scoreHome.innerText = totalHomeScore
+
+}
+
+function increaseByThreeHome(){
+
+    totalHomeScore = totalHomeScore+3
+    scoreHome.innerText = totalHomeScore
+
+}
+
+function increaseByFiveHome(){
+
+    totalHomeScore = totalHomeScore+5
+    scoreHome.innerText = totalHomeScore
+
+}
+
+
+
+//AWAY
+function increaseByOneAway(){
+
+    totalAwayScore = totalAwayScore+1
+    scoreAway.innerText = totalAwayScore
+
+}
+
+function increaseByThreeAway(){
+
+    totalAwayScore = totalAwayScore+3
+    scoreAway.innerText = totalAwayScore
+
+}
+
+function increaseByFiveAway(){
+
+    totalAwayScore = totalAwayScore+5
+    scoreAway.innerText = totalAwayScore
+
+}
+
+function Reset(){
+   scoreHome.innerText = 0;
+    scoreAway.innerText = 0;
+}
